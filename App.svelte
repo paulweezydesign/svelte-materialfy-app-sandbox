673b4149dc20db3fd635b9e05803a270093746a3
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2022-05-25 07:31:04 -0700

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { MaterialApp, Button } from "svelte-materialify";
+  import AppBar from "./AppBar.svelte";
+  import colors from "svelte-materialify/src/utils/colors";
+  import Button1 from "./Button.svelte";
+  const theme = "light";
+</script>
+
+
+
+<style>
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 40vh;
+  }
+  h1 {
+    text-align: center;
+    font-weight: 700;
+  }
+</style>
+
+<MaterialApp {theme}>
+	<AppBar />
+  <div>
+  <h1>Svelte Material app</h1>
+  </div>
+  <div>
+  <Button1 />
+  </div>
+</MaterialApp>
---
+++ AppBar.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { MaterialApp, AppBar, Button, Icon } from "svelte-materialify";
+  import { mdiMenu, mdiDotsVertical } from "@mdi/js";
+  let collapsed=false
+</script>
+
+
+  <AppBar {collapsed} class="primary-color theme--dark">
+  <div slot="icon">
+    <Button fab depressed text>
+      <Icon path={mdiMenu} />
+    </Button>
+  </div>
+  <span slot="title">Cloud Kickstarter</span>
+<div style="flex-grow:1" />
+<Button>Login</Button>
+  </AppBar>
+
